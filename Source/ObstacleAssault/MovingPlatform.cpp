e02b9cbbb4a0eec6af53c28b546077332ba00ed0
abs990 <abhi.snair0@gmail.com>
2025-09-01 22:00:58 -0700

    Sample ue log for int, float and string

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -6,7 +6,7 @@
 // Sets default values
 AMovingPlatform::AMovingPlatform()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -16,7 +16,11 @@ void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Display, TEXT("MovingPlatform BeginPlay"));
+	int NumberOfApples = 10;
+	float NumberOfHours = 1.5f;
+	FString MyName = "Abhishek";
+
+	UE_LOG(LogTemp, Display, TEXT("NumberOfApples: %d, NumberOfHours: %.2f, MyName: %s"), NumberOfApples, NumberOfHours, *MyName);
 }
 
 // Called every frame
